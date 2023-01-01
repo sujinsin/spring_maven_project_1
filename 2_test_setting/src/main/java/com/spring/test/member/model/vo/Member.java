917047b89ff36cfab186b23287698c9bed4fbdac
sujin sin <sinsu0051@naver.com>
2023-01-01 23:28:55 +0900

    member vo add

---
+++ 2_test_setting/src/main/java/com/spring/test/member/model/vo/Member.java
@@ -0,0 +1,30 @@
+package com.spring.test.member.model.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Builder
+public class Member {
+	private String id;
+	private String pwd;
+	private String name;
+	private String nickName;
+	private String email;
+	private String gender;
+	private int age;
+	private String phone;
+	private String address;
+	private Date enrollDate;
+	private Date updateDate;
+	private String memberStatus;
+	
+}
